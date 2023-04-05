4fe70dbd9e8c84512d358d3bea8c6d4e1716b61e
intgus <46197948+intGus@users.noreply.github.com>
2023-04-05 12:36:50 -0400

    Update App to include a shuffler function for the cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import Card from './components/Card'
 import Container from './components/Container'
+import { useEffect } from 'react'
 
 function App() {
   const [score, setScore] = useState({
     currentScore: 0,
     highScore: 0,
   })
-
   const [clickedCards, setClickedCards] = useState([])
+  const [shuffledArray, setShuffledArray] = useState([ 1,2,3,4,5,6,7,8 ])
 
   function updateCurrentScore() {
     setScore(prevCurrentScore => {
@@ -38,19 +39,28 @@ function App() {
     resetScore()
   }
 
+  useEffect(() => {
+    setShuffledArray([...shuffledArray].sort((a, b) => 0.5 - Math.random()));
+  }, [clickedCards])
+
+  const cards = shuffledArray.map((item) =>
+    <Card key={item.toString()} number={item} click={handleClick} />
+  )
+
   return (
     <div className="App">
       <div>Score: {score.currentScore}</div>
       <div>Best Score: {score.highScore}</div>
       <Container>
-        <Card number="1" click={handleClick} />
+        {cards}
+        {/* <Card number="1" click={handleClick} />
         <Card number="2" click={handleClick} />
         <Card color="white" number="3" click={handleClick} />
         <Card number="4" click={handleClick} />
         <Card number="5" click={handleClick} />
         <Card number="6" click={handleClick} />
         <Card number="7" click={handleClick} />
-        <Card color="yellow" number="8" click={handleClick} />
+        <Card color="yellow" number="8" click={handleClick} /> */}
       </Container>
     </div>
   )
